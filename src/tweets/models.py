788262bf67a68278f8d315f7c7ba388470b667de
권태형 <kwon5604@naver.com>
2018-01-19 10:13:16 +0900

    Retweet-3

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.urls import reverse
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -14,7 +15,13 @@ class TweetManager(models.Manager):
         else:
             original_parent = parent_obj
 
-        qs = self.objects.filter(user=user, parent=parent_obj)
+        qs = self.get_queryset().filter(
+            user=user, parent=original_parent
+            ).filter(
+                timestamp__year=timezone.now().year,
+                timestamp__month=timezone.now().month,
+                timestamp__day=timezone.now().day,
+                )
         if qs.exists():
             return None
 
